26282f1e829aa4e9b44d281791e497d085a3a161
נטע כנרי <netakanary@MacBook-Pro-sl-nt.local>
2025-11-18 20:01:19 +0200

    anagram

--- Anagram.java
+++ Anagram.java
@@ -2,48 +2,88 @@
 public class Anagram {
 	public static void main(String args[]) {
 		// Tests the isAnagram function.
-		System.out.println(isAnagram("silent","listen"));  // true
-		System.out.println(isAnagram("William Shakespeare","I am a weakish speller")); // true
-		System.out.println(isAnagram("Madam Curie","Radium came")); // true
-		System.out.println(isAnagram("Tom Marvolo Riddle","I am Lord Voldemort")); // true
+		System.out.println(isAnagram("silent", "listen")); // true
+		System.out.println(isAnagram("William Shakespeare", "I am a weakish	speller")); // true
+		System.out.println(isAnagram("Madam Curie", "Radium came")); // true
+		System.out.println(isAnagram("Tom Marvolo Riddle", "I am Lord Voldemort"));
+		// true
 
-		// Tests the preProcess function.
+		// // Tests the preProcess function.
 		System.out.println(preProcess("What? No way!!!"));
-		
+
 		// Tests the randomAnagram function.
-		System.out.println("silent and " + randomAnagram("silent") + " are anagrams.");
-		
-		// Performs a stress test of randomAnagram 
+		System.out.println("silent and " + randomAnagram("silent") + " are  anagrams.");
+
+		// Performs a stress test of randomAnagram
 		String str = "1234567";
 		Boolean pass = true;
-		//// 10 can be changed to much larger values, like 1000
+		// 10 can be changed to much larger values, like 1000
 		for (int i = 0; i < 10; i++) {
 			String randomAnagram = randomAnagram(str);
 			System.out.println(randomAnagram);
 			pass = pass && isAnagram(str, randomAnagram);
-			if (!pass) break;
+			if (!pass)
+				break;
 		}
 		System.out.println(pass ? "test passed" : "test Failed");
-	}  
+	}
 
 	// Returns true if the two given strings are anagrams, false otherwise.
 	public static boolean isAnagram(String str1, String str2) {
-		// Replace the following statement with your code
-		return false;
+		String cleanStr1 = preProcess(str1);
+		String cleanStr2 = preProcess(str2);
+		if (cleanStr1.length() != cleanStr2.length())
+			return false;
+		for (int i = 0; i < cleanStr2.length(); i++) {
+			if (timesCharAtStr(cleanStr1.charAt(i), cleanStr2) != timesCharAtStr(cleanStr1.charAt(i), cleanStr1))
+				return false;
+		}
+
+		return true;
 	}
-	   
-	// Returns a preprocessed version of the given string: all the letter characters are converted
-	// to lower-case, and all the other characters are deleted, except for spaces, which are left
+
+	public static int timesCharAtStr(char c, String str) {
+		if (str.indexOf(c) == -1)
+			return 0;
+
+		int cnt = 0;
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) == c) {
+				cnt++;
+			}
+		}
+		return cnt;
+	}
+
+	// Returns a preprocessed version of the given string: all the letter characters
+	// are converted
+	// to lower-case, and all the other characters are deleted, except for spaces,
+	// which are left
 	// as is. For example, the string "What? No way!" becomes "whatnoway"
 	public static String preProcess(String str) {
-		// Replace the following statement with your code
-		return "";
-	} 
-	   
-	// Returns a random anagram of the given string. The random anagram consists of the same
-	// characters as the given string, re-arranged in a random order. 
+		String tmpStr = "";
+		for (int i = 0; i < str.length(); i++) {
+			char currentChar = str.charAt(i);
+			if (currentChar >= 65 && currentChar <= 90) {
+				tmpStr += (char) (currentChar + 32);
+			} else if (currentChar >= 97 && currentChar <= 122) {
+				tmpStr += (char) currentChar;
+			}
+		}
+		return tmpStr;
+	}
+
+	// Returns a random anagram of the given string. The random anagram consists of
+	// the same
+	// characters as the given string, re-arranged in a random order.
 	public static String randomAnagram(String str) {
-		// Replace the following statement with your code
-		return "";
+		String mixStr = "";
+		String tmpStr = str;
+		for (int i = 0; i < str.length(); i++) {
+			int randomNum = (int) (Math.random() * (tmpStr.length()));
+			mixStr += tmpStr.charAt(randomNum);
+			tmpStr = tmpStr.substring(0, randomNum) + tmpStr.substring(randomNum + 1);
+		}
+		return mixStr;
 	}
 }
--- LoanCalc.java
+++ LoanCalc.java
@@ -1,54 +1,63 @@
 // Computes the periodical payment necessary to pay a given loan.
 public class LoanCalc {
-	
-	static double epsilon = 0.001;  // Approximation accuracy
-	static int iterationCounter;    // Number of iterations 
-	
+
+	static double epsilon = 0.001; // Approximation accuracy
+	static int iterationCounter; // Number of iterations
+
 	// Gets the loan data and computes the periodical payment.
-    // Expects to get three command-line arguments: loan amount (double),
-    // interest rate (double, as a percentage), and number of payments (int).  
-	public static void main(String[] args) {		
+	// Expects to get three command-line arguments: loan amount (double),
+	// interest rate (double, as a percentage), and number of payments (int).
+	public static void main(String[] args) {
 		// Gets the loan data
-		double loan = Double.parseDouble(args[0]);
-		double rate = Double.parseDouble(args[1]);
-		int n = Integer.parseInt(args[2]);
+		// double loan = Double.parseDouble(args[0]);
+		// double rate = Double.parseDouble(args[1]);
+		// int n = Integer.parseInt(args[2]);
+		double loan = 100000;
+		double rate = 5;
+		int n = 10;
 		System.out.println("Loan = " + loan + ", interest rate = " + rate + "%, periods = " + n);
+		System.out.println("endbalabce: " + endBalance(loan, rate, n, 10000));
 
-		// Computes the periodical payment using brute force search
-		System.out.print("\nPeriodical payment, using brute force: ");
-		System.out.println((int) bruteForceSolver(loan, rate, n, epsilon));
-		System.out.println("number of iterations: " + iterationCounter);
+		// // Computes the periodical payment using brute force search
+		// System.out.print("\nPeriodical payment, using brute force: ");
+		// System.out.println((int) bruteForceSolver(loan, rate, n, epsilon));
+		// System.out.println("number of iterations: " + iterationCounter);
 
-		// Computes the periodical payment using bisection search
-		System.out.print("\nPeriodical payment, using bi-section search: ");
-		System.out.println((int) bisectionSolver(loan, rate, n, epsilon));
-		System.out.println("number of iterations: " + iterationCounter);
+		// // Computes the periodical payment using bisection search
+		// System.out.print("\nPeriodical payment, using bi-section search: ");
+		// System.out.println((int) bisectionSolver(loan, rate, n, epsilon));
+		// System.out.println("number of iterations: " + iterationCounter);
 	}
 
 	// Computes the ending balance of a loan, given the loan amount, the periodical
-	// interest rate (as a percentage), the number of periods (n), and the periodical payment.
-	private static double endBalance(double loan, double rate, int n, double payment) {	
-		// Replace the following statement with your code
-		return 0;
+	// interest rate (as a percentage), the number of periods (n), and the
+	// periodical payment.
+	private static double endBalance(double loan, double rate, int n, double payment) {
+		double sum = loan;
+		for (int i = 0; i < n; i++) {
+			sum = (sum - payment) * (rate / 100.0 + 1.0);
+		}
+
+		return sum;
 	}
-	
+
 	// Uses sequential search to compute an approximation of the periodical payment
 	// that will bring the ending balance of a loan close to 0.
 	// Given: the sum of the loan, the periodical interest rate (as a percentage),
 	// the number of periods (n), and epsilon, the approximation's accuracy
 	// Side effect: modifies the class variable iterationCounter.
-    public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {
+	public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {
 		// Replace the following statement with your code
 		return 0;
-    }
-    
-    // Uses bisection search to compute an approximation of the periodical payment 
+	}
+
+	// Uses bisection search to compute an approximation of the periodical payment
 	// that will bring the ending balance of a loan close to 0.
 	// Given: the sum of the loan, the periodical interest rate (as a percentage),
 	// the number of periods (n), and epsilon, the approximation's accuracy
 	// Side effect: modifies the class variable iterationCounter.
-    public static double bisectionSolver(double loan, double rate, int n, double epsilon) {  
-        // Replace the following statement with your code
+	public static double bisectionSolver(double loan, double rate, int n, double epsilon) {
+		// Replace the following statement with your code
 		return 0;
-    }
+	}
 }
